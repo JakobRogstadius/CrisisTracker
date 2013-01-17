d507a6bb5836ea966f9a903d4f2d34ab6eb8f8ea
Claudio Teixeira <claudioteixeira7@gmail.com>
2013-01-17 15:41:36 +0000

    Updated NewsList View

---
+++ Web/extjs-front-end/app/view/latestnews/NewsList.js
@@ -0,0 +1,9 @@
+Ext.define('CrisisTracker.view.latestnews.NewsList', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.newslist', 
+
+    initComponent: function() {
+		console.log('CrisisTracker.view.latestnews.NewsList.JS -> Initialized');
+	}
+	
+});
