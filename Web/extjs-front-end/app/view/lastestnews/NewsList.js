4852ebeb9f3bd1494538f59c0ed5594e7ea2b7f8
Claudio Teixeira <claudioteixeira7@gmail.com>
2013-01-17 15:42:20 +0000

    Removed file

--- Web/extjs-front-end/app/view/lastestnews/NewsList.js
+++
@@ -1,9 +0,0 @@
-Ext.define('CrisisTracker.view.latestnews.NewsList', {
-    extend: 'Ext.panel.Panel',
-    alias: 'widget.newslist', 
-
-    initComponent: function() {
-		console.log('CrisisTracker.view.latestnews.NewsList.JS -> Initialized');
-	}
-	
-});
